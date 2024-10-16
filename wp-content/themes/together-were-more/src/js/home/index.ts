509e896f7fe10439b9e441d070dd4db69fef03c8
KJ Roelke <kj.roelke@gmail.com>
2024-10-16 14:24:18 -0500

    update post preview card functionality

---
+++ wp-content/themes/together-were-more/src/js/home/index.ts
@@ -0,0 +1,49 @@
+import '../../styles/pages/home.scss';
+
+class PostPreviewHandler {
+	private postPreviewContainer: HTMLElement | null;
+
+	constructor( containerId: string ) {
+		this.postPreviewContainer = document.getElementById( containerId );
+		this.initEventListeners();
+	}
+
+	private initEventListeners() {
+		if ( ! this.postPreviewContainer ) {
+			return;
+		}
+		this.postPreviewContainer.addEventListener( 'mouseover', ( ev ) =>
+			this.handleEvent( ev, 0.15 )
+		);
+		this.postPreviewContainer.addEventListener( 'mouseout', ( ev ) =>
+			this.handleEvent( ev, 0.5 )
+		);
+	}
+
+	private handleEvent( ev: MouseEvent | FocusEvent, opacity: number ) {
+		const overlay = this.selectOverlay( ev );
+		if ( overlay ) {
+			this.updateOpacity( overlay, opacity );
+		}
+	}
+
+	private selectOverlay( ev: MouseEvent | FocusEvent ): HTMLElement | null {
+		const target = ev.target as HTMLElement;
+		const postPreviewCard = target.closest(
+			'.post-preview-card'
+		) as HTMLElement;
+
+		const overlay = postPreviewCard
+			? ( postPreviewCard.querySelector(
+					'.post-preview-card__overlay'
+			  ) as HTMLElement )
+			: null;
+		return overlay;
+	}
+
+	private updateOpacity( overlay: HTMLElement, opacity: number ) {
+		overlay.style.setProperty( '--bs-bg-opacity', opacity.toString() );
+	}
+}
+
+new PostPreviewHandler( 'category-spotlight' );
--- wp-content/themes/together-were-more/template-parts/card-post-preview.php
+++ wp-content/themes/together-were-more/template-parts/card-post-preview.php
@@ -6,19 +6,21 @@
  */
 
 ?>
-<div class='post-preview-card d-flex flex-column'>
-	<figure class='card-cover position-relative mb-0 ratio ratio-16x9 d-flex flex-column justify-content-end'>
+<div class="post-preview-card d-flex flex-column h-100 position-relative">
+	<figure class="post-preview-card__cover mb-0 ratio ratio-16x9">
 		<?php the_post_thumbnail( 'profile-preview-card', array( 'class' => 'w-100 object-fit-cover' ) ); ?>
-		<div class='overlay bg-dark bg-opacity-75 w-100 h-100 z-1'></div>
-		<figcaption class='card-title text-white position-relative z-2 px-3'>
-			<h3 class='fs-2 fw-bold text-uppercase'>
+		<div class="post-preview-card__overlay bg-dark bg-opacity-50 w-100 h-100 z-1"></div>
+		<figcaption class="h-auto text-white position-relative z-2 px-3">
+			<h3 class="fs-2 fw-bold text-uppercase">
 				<?php the_title(); ?>
 			</h3>
-			<p class='fs-5 text-uppercase'><?php echo get_field( 'meta' )['title']; ?></p>
+			<p class="fs-5 text-uppercase mb-0">
+				<?php echo get_field( 'meta' )['title']; ?>
+			</p>
 		</figcaption>
 	</figure>
-	<div class='post-preview-card__body text-white px-3'>
-		<p class='mb-5'>
+	<div class="post-preview-card__body text-white p-3 d-flex flex-column h-100 z-2">
+		<p class="mb-5">
 			<?php the_field( 'archive_content' ); ?>
 		</p>
 		<?php
@@ -31,7 +33,7 @@
 		);
 
 		$button_element = 'a';
-		$button_classes = "btn btn-outline-{$color_overlays[ $category[0]->name ]} text-uppercase mb-3 fs-6";
+		$button_classes = "btn btn-outline-{$color_overlays[ $category[0]->name ]} text-uppercase mt-auto align-self-start fs-6";
 		$pronouns       = get_field( 'meta' )['pronouns'];
 		$button_text    = "See {$pronouns} Story";
 		$href           = "href='" . get_the_permalink() . "'";
@@ -40,8 +42,11 @@
 			$button_element = 'button';
 			$button_text   .= " in {$scheduled_date}";
 			$href           = null;
+
+		} else {
+			$button_classes .= ' stretched-link';
 		}
-		echo "<{$button_element} " . ( $href ?? '' ) . " class='{$button_classes}'>{$button_text}</{$button_element}>";
+		echo "<{$button_element} " . ( $href ?? '' ) . " class='{$button_classes}' " . ( 'button' === $button_element ? 'disabled' : '' ) . ">{$button_text}</{$button_element}>";
 		?>
 	</div>
 </div>
