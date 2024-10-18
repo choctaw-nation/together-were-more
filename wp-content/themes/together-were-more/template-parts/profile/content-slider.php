0a3ba02b78f303fb010342f42c59b32da93e4fcd
KJ Roelke <kj.roelke@gmail.com>
2024-10-18 16:43:31 -0500

    add swipe text

--- wp-content/themes/together-were-more/template-parts/profile/content-slider.php
+++ wp-content/themes/together-were-more/template-parts/profile/content-slider.php
@@ -47,6 +47,7 @@ new Asset_Loader( 'profile-swiper', Enqueue_Type::both, 'pages' );
 						?>
 					</div>
 				</div>
+				<span class="font-script position-absolute display-3" id="swipe-text" data-aos='fade-in'>Swipe -></span>
 			</div>
 			<div data-hash='slide2' class='swiper-slide h-100'>
 				<?php
