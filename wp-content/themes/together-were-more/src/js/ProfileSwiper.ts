2f34f4858a537ff8208368f00513b91f153a9313
kjroelke <kj.roelke@gmail.com>
2024-11-01 11:42:00 -0500

    finish 1.1.0

--- wp-content/themes/together-were-more/src/js/ProfileSwiper.ts
+++ wp-content/themes/together-were-more/src/js/ProfileSwiper.ts
@@ -1,5 +1,11 @@
 import Swiper from 'swiper';
-import { A11y, Mousewheel, FreeMode, Pagination } from 'swiper/modules';
+import {
+	A11y,
+	Mousewheel,
+	FreeMode,
+	Pagination,
+	Navigation,
+} from 'swiper/modules';
 
 // Styles
 import 'swiper/scss';
@@ -7,29 +13,71 @@ import 'swiper/scss/a11y';
 import 'swiper/scss/mousewheel';
 import 'swiper/scss/free-mode';
 import 'swiper/scss/pagination';
+import 'swiper/scss/navigation';
 import '../styles/components/single/swiper.scss';
 import { bounceSwiper } from './bounceSwiper';
+import { bootstrapBreakpoints } from './utilities';
 
-new Swiper( '.swiper', {
-	modules: [ A11y, Mousewheel, FreeMode, Pagination ],
-	slidesPerView: 1,
-	direction: 'horizontal',
-	spaceBetween: 0,
-	autoHeight: true,
-	grabCursor: true,
-	loop: false,
-	freeMode: {
-		enabled: true,
-		sticky: true,
-	},
-	mousewheel: {
-		enabled: true,
-		forceToAxis: true,
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-} );
-
-bounceSwiper( 'profile-swiper', '.swiper', 'bounce-left-small' );
+const profileSwiper = document.querySelector< HTMLElement >(
+	'#profile-swiper .swiper'
+);
+if ( profileSwiper ) {
+	new Swiper( profileSwiper, {
+		modules: [ A11y, Mousewheel, FreeMode, Pagination ],
+		slidesPerView: 1,
+		direction: 'horizontal',
+		spaceBetween: 0,
+		autoHeight: true,
+		grabCursor: true,
+		loop: false,
+		freeMode: {
+			enabled: true,
+			sticky: true,
+		},
+		mousewheel: {
+			enabled: true,
+			forceToAxis: true,
+		},
+		pagination: {
+			el: '.profile-swiper-pagination',
+			clickable: true,
+		},
+	} );
+	bounceSwiper( 'profile-swiper', '.swiper', 'bounce-left-small' );
+}
+const gallerySwiper =
+	document.querySelector< HTMLElement >( '#gallery-swiper' );
+if ( gallerySwiper ) {
+	new Swiper( gallerySwiper, {
+		modules: [ A11y, Pagination, Navigation, Mousewheel ],
+		slidesPerView: 1,
+		direction: 'horizontal',
+		spaceBetween: 20,
+		autoHeight: true,
+		grabCursor: true,
+		loop: false,
+		rewind: true,
+		mousewheel: {
+			enabled: true,
+			forceToAxis: true,
+		},
+		pagination: {
+			el: '.gallery-swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.gallery-swiper-button-next',
+			prevEl: '.gallery-swiper-button-prev',
+		},
+		breakpoints: {
+			[ bootstrapBreakpoints[ 'md' ] ]: {
+				slidesPerView: 2,
+				slidesPerGroup: 2,
+			},
+			[ bootstrapBreakpoints[ 'lg' ] ]: {
+				slidesPerView: 3,
+				slidesPerGroup: 3,
+			},
+		},
+	} );
+}
--- wp-content/themes/together-were-more/src/js/single/index.ts
+++ wp-content/themes/together-were-more/src/js/single/index.ts
@@ -1,7 +1,7 @@
 import '../../styles/pages/single.scss';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
-import { bootstrapBreakpoints } from '../utilities';
+import ParallaxController from './ParallaxController';
 
 AOS.init( {
 	easing: 'ease-in-out',
@@ -9,38 +9,7 @@ AOS.init( {
 	duration: 550,
 } );
 
-document.addEventListener( 'DOMContentLoaded', function () {
-	if ( window.innerWidth < bootstrapBreakpoints.lg ) {
-		return;
-	}
-	const parallaxElements = document.querySelectorAll( '.parallax' );
-
-	const observerOptions = {
-		root: null, // Use the viewport as the root
-		rootMargin: '0px',
-		threshold: 0, // Trigger as soon as even one pixel is visible
-	};
-
-	const observer = new IntersectionObserver( ( entries ) => {
-		entries.forEach( ( entry ) => {
-			if ( entry.isIntersecting ) {
-				window.addEventListener( 'scroll', handleScroll );
-			}
-		} );
-	}, observerOptions );
-
-	parallaxElements.forEach( ( element ) => {
-		observer.observe( element );
-	} );
-
-	function handleScroll() {
-		parallaxElements.forEach( ( element ) => {
-			const speed = 0.5; // Adjust the speed of the parallax effect
-			const rect = element.getBoundingClientRect();
-			const offsetDistance = window.innerHeight - rect.bottom;
-			const offset =
-				offsetDistance * speed < 0 ? 0 : offsetDistance * speed;
-			element.style.transform = `translateY(${ offset }px)`;
-		} );
-	}
+const parallaxController = new ParallaxController();
+window.addEventListener( 'resize', () => {
+	parallaxController.setParallaxContainerHeights( true );
 } );
