fb301d8a90ce7a9775ae57cb4924635dd55eec6a
KJ Roelke <kroelke@choctawnation.com>
2025-01-07 15:07:23 -0600

    fix slide 5 image container

--- wp-content/themes/together-were-more/template-parts/profile/content-slider-slide-5.php
+++ wp-content/themes/together-were-more/template-parts/profile/content-slider-slide-5.php
@@ -6,7 +6,7 @@
  */
 
 $slide_5 = $args['slide_5'];
-echo "<div class='image-container p-4 p-md-5'>";
+echo "<div class='ratio ratio-1x1 w-75'>";
 echo wp_get_attachment_image(
 	$slide_5['image'],
 	'full',
