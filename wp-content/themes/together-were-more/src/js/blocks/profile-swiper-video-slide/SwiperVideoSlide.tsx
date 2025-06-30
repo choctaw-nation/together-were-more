d045acd2a5fce384a522763f71bf3ceda2d832c5
KJ Roelke <kroelke@choctawnation.com>
2025-06-30 12:47:26 -0500

    update block styles

--- wp-content/themes/together-were-more/src/js/blocks/profile-swiper-video-slide/SwiperVideoSlide.tsx
+++ wp-content/themes/together-were-more/src/js/blocks/profile-swiper-video-slide/SwiperVideoSlide.tsx
@@ -8,24 +8,17 @@ export default function SwiperVideoSlide( {
 	caption: string;
 } ) {
 	const { children, ...innerBlocksProps } = editorView
-		? useInnerBlocksProps(
-				{},
-				{
-					allowedBlocks: [
-						'cno-lite-vimeo/cno-plugin-lite-vimeo-block',
-					],
-					defaultBlock: {
-						name: 'cno-lite-vimeo/cno-plugin-lite-vimeo-block',
-					},
-					directInsert: true,
-					orientation: 'horizontal',
-					template: [
-						[ 'cno-lite-vimeo/cno-plugin-lite-vimeo-block' ],
-					],
-					renderAppender: false,
-					templateInsertUpdatesSelection: true,
-				}
-		  )
+		? useInnerBlocksProps( useBlockProps(), {
+				allowedBlocks: [ 'cno-lite-vimeo/cno-plugin-lite-vimeo-block' ],
+				defaultBlock: {
+					name: 'cno-lite-vimeo/cno-plugin-lite-vimeo-block',
+				},
+				directInsert: true,
+				orientation: 'horizontal',
+				template: [ [ 'cno-lite-vimeo/cno-plugin-lite-vimeo-block' ] ],
+				renderAppender: false,
+				templateInsertUpdatesSelection: true,
+		  } )
 		: useInnerBlocksProps.save( useBlockProps.save() );
 	const captionStyles = editorView ? {} : {};
 	return (
