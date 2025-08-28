5f13ec4bb018afcc042caf20c8064b2eb94ba0b0
KJ Roelke <kroelke@choctawnation.com>
2025-08-28 11:46:18 -0500

    add config files and update packages

---
+++ stylelint.config.mjs
@@ -0,0 +1,44 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: ['stylelint-config-standard-scss'],
+	rules: {
+		'color-named': 'always-where-possible',
+		'scss/at-function-pattern': [
+			'^(-?[a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+			{
+				message: 'Expected function name to be kebab-case',
+			},
+		],
+		'scss/at-mixin-pattern': [
+			'^(-?[a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+			{
+				message: 'Expected mixin name to be kebab-case',
+			},
+		],
+		'custom-property-pattern': [
+			'^--(wp--[a-z]+(--[a-z0-9]+)*)|([a-z]+(-[a-z0-9]+)*)$',
+			{
+				message:
+					'Custom properties must be kebab-case, or WordPress-style (--wp--preset--type--value)',
+			},
+		],
+	},
+	ignoreFiles: [
+		'wp-content/themes/**/src/styles/vendors/bootstrap.scss',
+		'wp-content/themes/**/src/styles/abstracts/_bs_breakpoints.scss',
+	],
+	overrides: [
+		{
+			files: ['**/*.scss'],
+			customSyntax: 'postcss-scss',
+		},
+		{
+			files: ['**/*.html', '**/*.php'],
+			customSyntax: 'postcss-html',
+		},
+		{
+			files: ['**/*.jsx', '**/*.tsx', '**/*.js', '**/*.ts'],
+			customSyntax: 'postcss-js',
+		},
+	],
+};
