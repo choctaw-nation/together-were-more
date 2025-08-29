b92bae93dd1bf3bbbf78e7070ce203a76bcd9f42
KJ Roelke <kroelke@choctawnation.com>
2025-08-29 12:00:55 -0500

    tweak spacing

--- wp-content/themes/together-were-more/template-parts/home/section-category-spotlight.php
+++ wp-content/themes/together-were-more/template-parts/home/section-category-spotlight.php
@@ -19,7 +19,16 @@ $bg_color  = cno_get_category_color( $spotlight->name );
 			<div class="row">
 				<div class="col-lg-4 text-center">
 					<h2 class="text-white display-2 text-uppercase"><?php echo $spotlight->name; ?></h2>
-					<?php get_template_part( 'template-parts/ui/hr', 'diamonds', array( 'color' => 'white' ) ); ?>
+					<?php
+					get_template_part(
+						'template-parts/ui/hr',
+						'diamonds',
+						array(
+							'color' => 'white',
+							'class' => 'mb-0',
+						)
+					);
+					?>
 				</div>
 			</div>
 		</div>
